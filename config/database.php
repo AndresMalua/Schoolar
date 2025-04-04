8b79d05194c1f344bf3d45f9a33999d9b5b26c43
Andres <maluaandres1@gmail.com>
2025-04-04 18:03:49 -0500

    supabase

--- config/database.php
+++ config/database.php
@@ -1,10 +1,19 @@
 <?php
     //Config connection
+    
+    $host       = "aws-0-us-east-2.pooler.supabase.com";
+    $port       = "6543";
+    $dbname     = "postgres";
+    $user       = "postgres.ijdheyyhaflvxefarigg";
+    $password   = "unicesmag@@";
+    
+    /*
     $host       = "localhost";
     $port       = "5432";
     $dbname     = "schoolar";
     $user       = "postgres";
     $password   = "unicesmag";
+    */
 
     //create connection 
     $conn = pg_connect("
